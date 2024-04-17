94c77d5cb920da65f5fd48405bf95bd3a00b0370
LKanan <lucienkanani@gmail.com>
2024-04-17 14:48:31 +0200

    migration du projet vers le projet django créé par l'IDE Pycharm

--- Formation_Django/urls.py
+++ Formation_Django/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Formation project.
+URL configuration for Formation_Django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
